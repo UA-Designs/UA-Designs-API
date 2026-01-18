1f063ef76f31b6988e1ed70a64444343060ff84b
kohicha <107725512+kohicha@users.noreply.github.com>
2026-01-18 18:02:00 +0800

    feat: added and refactored some of the fields to Budget model

--- src/models/Cost/Budget/index.js
+++ src/models/Cost/Budget/index.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     amount: {
-      type: DataTypes.DECIMAL(10, 2),
+      type: DataTypes.DECIMAL(15, 2),
       allowNull: false
     },
     currency: {
@@ -24,11 +24,97 @@ module.exports = (sequelize, DataTypes) => {
     description: {
       type: DataTypes.TEXT,
       allowNull: true
+    },
+    projectId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'projects',
+        key: 'id'
+      }
+    },
+    startDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    endDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    contingency: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: true,
+      defaultValue: 0,
+      comment: 'Contingency percentage'
+    },
+    managementReserve: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: true,
+      defaultValue: 0,
+      comment: 'Management reserve percentage'
+    },
+    createdBy: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      references: {
+        model: 'users',
+        key: 'id'
+      }
+    },
+    approvedBy: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      references: {
+        model: 'users',
+        key: 'id'
+      }
+    },
+    approvedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    approvalNotes: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    previousBudgetId: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      references: {
+        model: 'budgets',
+        key: 'id'
+      },
+      comment: 'Reference to previous budget version if revised'
+    },
+    revisionReason: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    closedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    closedBy: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      references: {
+        model: 'users',
+        key: 'id'
+      }
+    },
+    closureNotes: {
+      type: DataTypes.TEXT,
+      allowNull: true
     }
   }, {
     tableName: 'budgets',
     timestamps: true,
-    paranoid: true
+    paranoid: true,
+    indexes: [
+      { fields: ['projectId'] },
+      { fields: ['status'] },
+      { fields: ['createdBy'] }
+    ]
   });
 
   return Budget;
